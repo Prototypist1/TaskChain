cb0969db656dabe928dbd0df505755304b1665d2
Prototypist1 <ColinWielga@outlook.com>
2018-08-09 13:14:32 -0400

    started work on version 2

---
+++ TaskChain2/ActionAbleConcurrent.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Prototypist.TaskChain
+{
+    public class ActionAbleConcurrent<TValue>
+    {
+        private volatile object value;
+
+        private class Link
+        {
+            public Exception exception;
+            public readonly Func<TValue, TValue> func;
+            public ManualResetEventSlim manualReset = new ManualResetEventSlim();
+            public volatile Link next;
+
+            public Link(Func<TValue, TValue> func) => this.func = func ?? throw new ArgumentNullException(nameof(func));
+
+            internal void Wait()
+            {
+                manualReset.Wait();
+                if (exception != null) {
+                    throw exception;
+                }
+            }
+        }
+
+        private volatile Link endOfChain = new Link(x => x);
+        private const int RUNNING = 1;
+        private const int STOPPED = 0;
+        private int running = STOPPED;
+        private volatile Link startOfChain;
+        
+        public TValue GetValue()
+        {
+            return (TValue)value;
+        }
+
+        public  void SetValue(TValue value)
+        {
+            Act(x => value);
+        }
+
+        public void Act(Func<TValue, TValue> func) {
+            var link =new Link(func);
+
+            var oldEndofChain = endOfChain;
+            while (true)
+            {
+                if (Interlocked.CompareExchange(ref oldEndofChain.next, link, null) == null)
+                {
+                    Interlocked.CompareExchange(ref startOfChain, link, null);
+                    DoWork();
+                    return;
+                }
+                oldEndofChain = Interlocked.CompareExchange(ref endOfChain, oldEndofChain.next, oldEndofChain).next;
+            }
+
+            async void DoWork()
+            {
+                if (Interlocked.CompareExchange(ref running, RUNNING, STOPPED) == STOPPED)
+                {
+                    try
+                    {
+                        value = startOfChain.func((TValue)value);
+                        startOfChain.manualReset.Set();
+                    }
+                    catch (Exception e)
+                    {
+                        startOfChain.exception = e;
+                        startOfChain.manualReset.Set();
+                    }
+                    startOfChain = startOfChain.next;
+                    running = STOPPED;
+                    if (startOfChain != null && Interlocked.CompareExchange(ref running, RUNNING, STOPPED) == STOPPED)
+                    {
+                        await Task.Run(() =>
+                        {
+                            while (startOfChain != null && Interlocked.CompareExchange(ref running, RUNNING, STOPPED) == STOPPED)
+                            {
+                                do
+                                {
+                                    try
+                                    {
+                                        value = startOfChain.func((TValue)value);
+                                        startOfChain.manualReset.Set();
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        startOfChain.exception = e;
+                                        startOfChain.manualReset.Set();
+                                    }
+                                    startOfChain = startOfChain.next;
+                                } while (startOfChain != null);
+                                running = STOPPED;
+                            }
+                        });
+                    }
+                }
+                else
+                {
+                    link.Wait();
+                }
+            }
+        }
+    }
+}
---
+++ TaskChain2/Concurrent.cs
@@ -0,0 +1,15 @@
+﻿namespace Prototypist.TaskChain
+{
+    public class Concurrent<TValue> 
+    {
+        private volatile object value;
+
+        public TValue GetValue() {
+            return (TValue)value;
+        }
+
+        public void SetValue(TValue value) {
+            this.value = value;
+        }
+    }
+}
