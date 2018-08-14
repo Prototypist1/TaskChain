cb724dc0b6f11de18acc90403c782dad8ff60115
Prototypist1 <ColinWielga@outlook.com>
2018-08-13 23:25:59 -0400

    aw gs how does this even work

--- TaskChain/QueueingConcurrent.cs
+++ TaskChain/QueueingConcurrent.cs
@@ -5,22 +5,81 @@ using System.Threading.Tasks;
 namespace Prototypist.TaskChain
 {
 
-    public class QueueingConcurrent<TValue>
+    public class QueueingConcurrent
+    {
+
+    }
+
+
+    protected class CrossSyncLink
+    {
+        public class FirstComeFirstServe
+        {
+            public Task<object[]> main;
+        }
+
+        private readonly FirstComeFirstServe firstComeFirstServe;
+        private readonly Func<object[], object[]> func;
+        private readonly Task<object>[] inputs;
+        public volatile Link next;
+        private readonly int i;
+        public TaskCompletionSource<object> MyValue = new TaskCompletionSource<object>();
+        public TaskCompletionSource<TValue> taskCompletionSource = new TaskCompletionSource<TValue>();
+
+        public CrossSyncLink(FirstComeFirstServe firstComeFirstServe, Func<object[], object[]> func, Task<object>[] inputs, int i)
+        {
+            this.firstComeFirstServe = firstComeFirstServe ?? throw new ArgumentNullException(nameof(firstComeFirstServe));
+            this.func = func ?? throw new ArgumentNullException(nameof(func));
+            this.inputs = inputs ?? throw new ArgumentNullException(nameof(inputs));
+            this.i = i;
+        }
+
+        public async Task<TValue> Do(object value)
+        {
+
+            MyValue.SetResult(value);
+            var thing = new TaskCompletionSource<object[]>();
+            if (Interlocked.CompareExchange(ref firstComeFirstServe.main, thing.Task, null) == null)
+            {
+                try
+                {
+                    thing.SetResult(func(await Task.WhenAll(inputs)));
+                }
+                catch (Exception e)
+                {
+                    thing.SetException(e);
+                }
+            }
+            var res = (TValue)(await firstComeFirstServe.main)[i];
+            taskCompletionSource.SetResult(res);
+            return res;
+        }
+
+    }
+
+
+    public class QueueingConcurrent<TValue>: QueueingConcurrent
     {
         protected volatile object value;
 
         protected class Link
         {
-            public readonly Func<TValue, TValue> func;
+            private readonly Func<TValue, TValue> func;
             public volatile Link next;
             public TaskCompletionSource<TValue> taskCompletionSource = new TaskCompletionSource<TValue>();
-            public void Done(TValue res)
+            public Task<TValue> Do(TValue value)
             {
-                taskCompletionSource.SetResult(res);
-            }
-            public void Done(Exception e)
-            {
-                taskCompletionSource.SetException(e);
+                try
+                {
+                    var res = func(value);
+                    taskCompletionSource.SetResult(res);
+                    return taskCompletionSource.Task;
+                }
+                catch (Exception e)
+                {
+                    taskCompletionSource.SetException(e);
+                    return taskCompletionSource.Task;
+                }
             }
 
             public Link(Func<TValue, TValue> func) => this.func = func ?? throw new ArgumentNullException(nameof(func));
@@ -67,48 +126,34 @@ namespace Prototypist.TaskChain
                 }
             }
 
-            Task<TValue> DoWork()
+            async Task<TValue> DoWork()
             {
                 if (Interlocked.CompareExchange(ref running, RUNNING, STOPPED) == STOPPED)
                 {
-                    try
-                    {
-                        value = startOfChain.func((TValue)value);
-                        startOfChain.Done((TValue)value);
-                    }
-                    catch (Exception e)
-                    {
-
-                        startOfChain.Done(e);
-                    }
+                    value = await startOfChain.Do((TValue)value);
                     startOfChain = startOfChain.next;
                     running = STOPPED;
                     if (startOfChain != null && Interlocked.CompareExchange(ref running, RUNNING, STOPPED) == STOPPED)
                     {
-                        Task.Run(() =>
-                        {
-                            do
-                            {
-                                do
-                                {
-                                    try
-                                    {
-                                        value = startOfChain.func((TValue)value);
-                                        startOfChain.Done((TValue)value);
-                                    }
-                                    catch (Exception e)
-                                    {
-
-                                        startOfChain.Done(e);
-                                    }
-                                    startOfChain = startOfChain.next;
-                                } while (startOfChain != null);
-                                running = STOPPED;
-                            } while (startOfChain != null && Interlocked.CompareExchange(ref running, RUNNING, STOPPED) == STOPPED);
-                        });
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                        Task.Run(Process);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                     }
                 }
-                return link.taskCompletionSource.Task;
+                return await link.taskCompletionSource.Task;
+
+                async void Process()
+                {
+                    do
+                    {
+                        do
+                        {
+                            value = await startOfChain.Do((TValue)value);
+                            startOfChain = startOfChain.next;
+                        } while (startOfChain != null);
+                        running = STOPPED;
+                    } while (startOfChain != null && Interlocked.CompareExchange(ref running, RUNNING, STOPPED) == STOPPED);
+                }
             }
         }
     }
