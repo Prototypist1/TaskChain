61bcaa5dce815f8d760f8674f983fd02ca1b837a
Prototypist1 <ColinWielga@outlook.com>
2021-09-22 20:54:09 -0400

    handle null better

--- TaskChain/OptimisticConcurrent.cs
+++ TaskChain/OptimisticConcurrent.cs
@@ -7,17 +7,22 @@ namespace Prototypist.TaskChain
         where TValue : ICopy<TValue>
     { 
         
-        public class Envolope
+        public class Envelope
         {
             public readonly TValue value;
 
-            public Envolope(TValue value)
+            public Envelope(TValue value)
             {
                 this.value = value;
             }
         }
 
-        private Envolope value;
+        private Envelope value;
+
+        public OptimisticConcurrent(TValue value)
+        {
+            this.value = new Envelope(value);
+        }
 
         /// <summary>
         /// warning! update may be called multiple times!
@@ -26,7 +31,7 @@ namespace Prototypist.TaskChain
 
             while (true) {
                 var myView = value;
-                var res = new Envolope (update(myView.value.Copy()) );
+                var res = new Envelope (update(myView.value == null ? default : myView.value.Copy()) );
                 if (Interlocked.CompareExchange(ref value, res, myView) == myView) {
                     return res.value;
                 }
